9ba4426f8f75f537538ebe3881da310d21aceba8
alwaysmsdfan7 <amansingh8174922@gmail.com>
2024-09-26 07:52:10 +0000

    02_scopes.js

---
+++ 03_basic/02_scopes.js
@@ -0,0 +1,63 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true){
+    const username = "Aman"
+    if (username === "Aman"){
+        const website = "youtube"
+        // console.log(username + website);
+    }
+// console.log(website);
+}
+
+// console.log(username);
+
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++intersecting++++++++++++++++++++++++++++++++++++++++
+
+
+console.log(addone(5));
+
+function  addone (num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+
+}
+
