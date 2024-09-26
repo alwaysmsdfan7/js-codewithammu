e263eac8e282da3694249e1c702ab43b5cf5f4f0
alwaysmsdfan7 <amansingh8174922@gmail.com>
2024-09-26 09:37:51 +0000

    04_iife.js

---
+++ 03_basic/04_iife.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function ammu(){
+    console.log(`DB CONNECTED`);
+// named IIFE
+})();
+
+
+((name) => {
+console.log(`DB CONNECTED TWO ${name}`);
+
+})('Aman')
