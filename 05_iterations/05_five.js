d763a83c2642cd3f23026d29dc49ac20e2e10a07
alwaysmsdfan7 <amansingh8174922@gmail.com>
2024-09-27 13:18:48 +0000

    05_five.js

---
+++ 05_iterations/05_five.js
@@ -0,0 +1,41 @@
+const coding =  ["js", "rb", "py", "java", "cpp", "python"]
+
+// coding.forEach(  function (val) {
+//     console.log(val);
+
+// })
+// coding.forEach((item) =>{
+//     console.log(item);
+// })
+
+// function printMe(item){
+//     console.log(item)
+// }
+// coding.forEach(printMe)
+
+
+// coding.forEach((item, index, arr) => {
+// console.log(item,index, arr);
+// });
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+
+    {
+        languageName: "python",
+        languageFileName: "python"
+    },
+]
+
+
+myCoding.forEach((item) =>{
+    console.log(item.languageName);
+})
